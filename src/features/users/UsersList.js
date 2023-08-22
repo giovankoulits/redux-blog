175eac2459d7d86ca2837cd47c6221a0f6df925a
chupacambra <jovankoul@hotmail.com>
2023-08-22 20:14:19 +0300

    Added EntityAdapter and getSelectors

--- src/features/users/UsersList.js
+++ src/features/users/UsersList.js
@@ -4,14 +4,17 @@ import { Link } from 'react-router-dom';
 
 const UsersList = () => {
   const users = useSelector(selectAllUsers);
+
   const renderedUsers = users.map((user) => (
     <li key={user.id}>
-      <Link to={`user/${user.id}`}>{user.name}</Link>
+      <Link to={`/user/${user.id}`}>{user.name}</Link>
     </li>
   ));
+
   return (
     <section>
       <h2>Users</h2>
+
       <ul>{renderedUsers}</ul>
     </section>
   );
